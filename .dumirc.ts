6adc9be22f770d7dc8642899c89a2931f0826004
wjx7794 <wjx7794@163.com>
2024-04-25 12:59:59 +0800

    feat: 调整配置

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'dumi';
 
+// github 仓库名称
+const defaultPath = '/dumiAndFather/';
+// 打包后 gh-pages 会默认在路径上拼接仓库名称
+const baseUrl = process.env.NODE_ENV === 'production' ? defaultPath : '/';
+
 export default defineConfig({
-  base: '/dumiAndFather/',
-  publicPath: '/dumiAndFather/',
+  base: baseUrl,
+  publicPath: baseUrl,
   outputPath: 'docs-dist',
   // 主题配置项
   themeConfig: {
